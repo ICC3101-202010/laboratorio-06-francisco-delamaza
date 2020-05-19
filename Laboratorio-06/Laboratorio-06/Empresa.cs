1456892ec5fd91c1314f294ea576d1e9c593a32b
Francisco De la Maza <fjdelamaza@miuandes.cl>
2020-05-19 13:22:28 -0400

    Parte 1 Lista

---
+++ Laboratorio-06/Laboratorio-06/Empresa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_06
+{
+    [Serializable]
+    class Empresa
+    {
+        public string nombre;
+        public string rut;
+
+        public Empresa(string nombre, string rut)
+        {
+            this.nombre = nombre;
+            this.rut = rut;
+        }
+    }
+}
--- Laboratorio-06/Laboratorio-06/Program.cs
+++ Laboratorio-06/Laboratorio-06/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
 
 namespace Laboratorio_06
 {
@@ -10,6 +15,70 @@ namespace Laboratorio_06
     {
         static void Main(string[] args)
         {
+            bool x = true;
+            IFormatter formatter = new BinaryFormatter();
+            Console.WriteLine("Bienvenido");
+            while (x == true)
+            {
+                
+                Console.WriteLine("Desea utilizar archivo para cargar?(Y/N)");
+                string respuesta = Console.ReadLine();
+                switch (respuesta)
+                {
+                    case "Y":
+                        Console.Clear();
+                        try
+                        {
+                            Stream streamp = new FileStream("empresa.bin", FileMode.Open, FileAccess.Read);
+                            Empresa newemp = (Empresa)formatter.Deserialize(streamp);
+                            Console.WriteLine(newemp.nombre);
+                            Console.WriteLine(newemp.rut);
+                            streamp.Close();
+                        }
+
+
+                        catch (FileNotFoundException e)
+                        {
+
+                            Console.WriteLine(e.Message);
+                            Thread.Sleep(2000);
+                            goto case "N";
+                                
+
+                        }
+
+                        break;
+                        
+
+                    case "N":
+
+                        Console.Clear();
+                        Console.WriteLine("Ingrese nombre de la empresa:");
+                        string nombre = Console.ReadLine();
+                        Console.WriteLine("Ingrese rut de la empresa");
+                        string rut = Console.ReadLine();
+                        Empresa emp = new Empresa(nombre, rut);
+
+                        Stream stream = new FileStream("empresa.bin", FileMode.Create, FileAccess.Write);
+                        formatter.Serialize(stream, emp);
+                        stream.Close();
+                        Thread.Sleep(1000);
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Respuesta no valida");
+                        break;
+
+
+
+
+                } 
+                    
+            
+
+            }
+
         }
     }
 }
